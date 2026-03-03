d79d49172df1bf8328ce512a7f56fe6620627d5d
tokareva-stacy <tokarevanyura@gmail.com>
2026-03-03 15:15:30 +0200

    add Pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,58 @@
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch } from "../../app/store";
+import { setPage } from "../../features/todos/todosSlice";
+import {
+  selectFilters,
+  selectTotalPages,
+} from "../../features/todos/todosSelectors";
+import styles from "./Pagination.module.scss";
+
+const Pagination = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { page } = useSelector(selectFilters);
+  const totalPages = useSelector(selectTotalPages);
+
+  if (totalPages <= 1) return null;
+
+  const handlePrev = () => {
+    if (page > 1) dispatch(setPage(page - 1));
+  };
+
+  const handleNext = () => {
+    if (page < totalPages) dispatch(setPage(page + 1));
+  };
+
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  return (
+    <div className={styles.wrapper}>
+      <button
+        onClick={handlePrev}
+        disabled={page === 1}
+        className={styles.button}
+      >
+        Prev
+      </button>
+
+      {pages.map((p) => (
+        <button
+          key={p}
+          onClick={() => dispatch(setPage(p))}
+          className={`${styles.button} ${p === page ? styles.active : ""}`}
+        >
+          {p}
+        </button>
+      ))}
+
+      <button
+        onClick={handleNext}
+        disabled={page === totalPages}
+        className={styles.button}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
