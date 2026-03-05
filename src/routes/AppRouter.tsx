818ec805d2f440c8afe97ccad5a70c4a2d1558bb
tokareva-stacy <tokarevanyura@gmail.com>
2026-03-05 14:51:08 +0200

    automatic redirect to /todos

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,12 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 import TodosPage from "../pages/TodosPage";
 import TodoDetailsPage from "../pages/TodoDetailsPage";
 
 export const AppRouter = () => (
   <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Navigate to="/todos" replace />} />
       <Route path="/todos" element={<TodosPage />} />
       <Route path="/todos/:id" element={<TodoDetailsPage />} />
+      <Route
+        path="*"
+        element={
+          <div>
+            Whoops!...Page not found. <Link to="/todos">Return to tasks</Link>
+          </div>
+        }
+      />
     </Routes>
   </BrowserRouter>
 );
